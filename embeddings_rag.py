31d8af2206f016d3df86f3cf0a03864c2277920e
Udita Singh <udita1294@gmail.com>
2026-08-12 02:26:08 +0530

    Cosine similarity calculation function implemented

--- embeddings_rag.py
+++ embeddings_rag.py
@@ -5,9 +5,20 @@ from groq import Groq
 import numpy as np
 from sentence_transformers import SentenceTransformer
 
+def cosine_similarity(a,b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
 model = SentenceTransformer("all-MiniLM-L6-v2")
 text = "Machine Learning is fun"
 
 embedding = model.encode(text)
-print(embedding.shape)  # Output: (384)
-print(embedding[:10])  # Output: [0.12345678 0.23456789 ...] (example values)
+# print(embedding.shape)  # Output: (384)
+# print(embedding[:10])  # Output: [0.12345678 0.23456789 ...] (example values)
+
+t1 = "There are 24 paid leaves"
+t2 = "There are 24 vacation days"
+
+v1 = model.encode(t1)
+v2 = model.encode(t2)
+
+print("Cosine similarity between t1 and t2:", cosine_similarity(v1, v2))
